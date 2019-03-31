ab0d3077e4489cf597286b279b386f4d3eb61e44
Sipun <Sipun@DESKTOP-QM1B3SB>
2019-03-31 19:21:33 +0530

    dev1 count

--- PersistanceLogicOperation/src/com/nt/DAO/Dao.java
+++ PersistanceLogicOperation/src/com/nt/DAO/Dao.java
@@ -2,12 +2,20 @@ package com.nt.DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Dao {
 	public static void getConnection(String url,String user,String password)throws SQLException {
 		Connection con=null;
-		con=DriverManager.getConnection(url, user, password);
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		con=DriverManager.getConnection(jdbc:oracle:thin:@localhost, scott, tiger);
+		ps=con.prepareStatement("select count(*) from emp");
+		rs=ps.executeQuery();
+		rs.next();
+		System.out.println(rs.getString(1));
 		
 		
 	}
