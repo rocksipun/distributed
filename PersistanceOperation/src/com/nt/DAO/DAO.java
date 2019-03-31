2c46dda05e298945d1e427ad1cceaa7dc845b0d1
Sipun <Sipun@DESKTOP-QM1B3SB>
2019-03-31 19:54:32 +0530

    dev2

--- PersistanceOperation/src/com/nt/DAO/DAO.java
+++ PersistanceOperation/src/com/nt/DAO/DAO.java
@@ -7,7 +7,14 @@ import java.sql.SQLException;
 public class DAO {
 	public static void getConn(String url,String user,String password)throws SQLException {
 		Connection con=null;
-		con=DriverManager.getConnection(url, user, password);
+		preparedStatement ps=null;
+		ResultSet rs=null;
+		dao.getConnectio("jdbc:oracle:thin:@localhost:orcl", "scott", "tiger");
+		ps=con.prepareStatement("select count(*) from emp");
+		rs=ps.executeQuery();
+		rs.next();
+		System.out.println(rs.getString(1));
+		
 	}
 
 }
--- PersistanceOperation/src/com/nt/test/Testing.java
+++ PersistanceOperation/src/com/nt/test/Testing.java
@@ -4,6 +4,8 @@ public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DAO dao=null
+				dao.getConn();
 
 	}
 
